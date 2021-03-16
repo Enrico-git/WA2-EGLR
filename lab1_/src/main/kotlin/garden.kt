c935ab9b81d1402dbfa80f2041726019d20496e5
luigideluca <basketteur860@gmail.com>
2021-03-16 14:22:38 +0100

    lab1: first three exercises done, fourth started

---
+++ lab1_/src/main/kotlin/garden.kt
@@ -0,0 +1,10 @@
+class KindergartenGarden(private val diagram: String) {
+    fun getPlantsOfStudent(student: String): List<String>? {
+        // TODO: implement me
+        return null
+    }
+}
+
+fun main() {
+
+}
