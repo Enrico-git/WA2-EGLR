12c8a71843c44abae3570f4d12d5714a2c8af385
Enrico Alberti <enrico.alberti.18@gmail.com>
2021-04-15 15:16:03 +0200

    used sequence instead of table for issue #8

--- ecommerce/src/main/resources/data.sql
+++ ecommerce/src/main/resources/data.sql
@@ -2,13 +2,11 @@
 -- -- 3 customers
 -- -- 3 wallets (1 per customer)
 -- -- 2 transaction (from Customer1 to Customer2, from Customer1 to Customer3)
-insert into customer(id, name, surname, address, email) values(1, "Alice", "Pleasance Liddell", "Wonderland, 2", "alice_inwonderland@mail.com");
-insert into customer(id, name, surname, address, email) values(2, "The March", "Hare", "Dining Table, 1", "march_hare@mail.com");
-insert into customer(id, name, surname, address, email) values(3, "The Mad", "Hatter", "Dining Table, 2", "mad_hatter@mail.com");
-insert into wallet(id, balance, customer) values(4, 1000, 1);
-insert into wallet(id, balance, customer) values(5, 750, 2);
-insert into wallet(id, balance, customer) values(6, 500, 3);
-insert into transaction(id, amount, timestamp, sender, receiver) values (7, 50, "2020-01-01 00:00:00", 4, 5);
-insert into transaction(id, amount, timestamp, sender, receiver) values (8, 250, "2020-01-01 00:00:00", 4, 6);
-
-update pk_sequence set next_val = 9;
+insert into customer(id, name, surname, address, email) values(NEXTVAL(pk_sequence), "Alice", "Pleasance Liddell", "Wonderland, 2", "alice_inwonderland@mail.com");
+insert into customer(id, name, surname, address, email) values(NEXTVAL(pk_sequence), "The March", "Hare", "Dining Table, 1", "march_hare@mail.com");
+insert into customer(id, name, surname, address, email) values(NEXTVAL(pk_sequence), "The Mad", "Hatter", "Dining Table, 2", "mad_hatter@mail.com");
+insert into wallet(id, balance, customer) values(NEXTVAL(pk_sequence), 1000, 1);
+insert into wallet(id, balance, customer) values(NEXTVAL(pk_sequence), 750, 2);
+insert into wallet(id, balance, customer) values(NEXTVAL(pk_sequence), 500, 3);
+insert into transaction(id, amount, timestamp, sender, receiver) values (NEXTVAL(pk_sequence), 50, "2020-01-01 00:00:00", 4, 5);
+insert into transaction(id, amount, timestamp, sender, receiver) values (NEXTVAL(pk_sequence), 250, "2020-01-01 00:00:00", 4, 6);
