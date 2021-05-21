dadc781139b636fe3e7cd42c6455a6e812773897
thepassenger <thepassenger@debian-laptop.home>
2021-05-21 19:16:01 +0200

    add single query in addCommentToProduct

--- lab_5/services/ProductService.js
+++ lab_5/services/ProductService.js
@@ -3,7 +3,7 @@ import Product from "../models/Product.js"
 import mongoose from "mongoose"
 
 export const getProductById = (id, numOfComments = 0) => {
-    return Product.findOne({_id: id}, {comments:  {$slice: Number(-numOfComments)}}).exec()
+    return Product.findOne({ _id: id }, { comments: { $slice: Number(-numOfComments) } }).exec()
 }
 
 export const getProducts = (filter, sort, numOfComments = 0) => {
@@ -17,24 +17,54 @@ export const getProducts = (filter, sort, numOfComments = 0) => {
         price: {
             $gte: filter.minPrice,
             $lte: filter.maxPrice
-        }},
-        {comments: {$slice: Number(-numOfComments)}
-    })
-    .sort ({
-        [sort.value]: sort.order
-    })
-    .exec()
+        }
+    },
+        {
+            comments: { $slice: Number(-numOfComments) }
+        })
+        .sort({
+            [sort.value]: sort.order
+        })
+        .exec()
 }
 
 export const addProduct = product => {
-        return Product.create({_id: mongoose.Types.ObjectId(), ...product, createdAt: new Date(), stars: 0, comments: []})
+    return Product.create({ _id: mongoose.Types.ObjectId(), ...product, createdAt: new Date(), stars: 0, comments: [] })
 }
 
-export const addCommentToProduct = async (productID, commentID, stars) => {
-    const product = await Product.findById(productID).exec()
-    const numOfComments = product.comments.length
-    return product.update({
-        $push: {comments: commentID}, 
-        stars: ((numOfComments*product.stars + stars) / (numOfComments+1))
-    }).exec()
+export const addCommentToProduct = (productID, commentID, stars) => {
+    // const product = await Product.findById(productID).exec()
+    // const numOfComments = product.comments.length
+    return Product.findByIdAndUpdate(productID,
+        [{
+            $set: {
+                comments: {
+                    $concatArrays: ["$comments", [commentID]]
+                },
+                stars: {
+                    $let: {
+                        vars: {
+                            len: { $size: "$comments" }
+                        },
+                        in: {
+                            $divide: [
+                                {
+                                    $sum: [
+                                        { $multiply: ["$stars", "$$len"] }, stars]
+                                },
+                                {
+                                    $sum: ["$$len", 1]
+                                }
+                            ]
+                        }
+                    }
+                }
+            }
+        }
+        ]
+        ).exec()
+    // return product.update({
+        // $push: {comments: commentID}, 
+        // stars: ((numOfComments*product.stars + stars) / (numOfComments+1))
+    // }).exec()
 }
