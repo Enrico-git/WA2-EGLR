0a6858e757e924b19fa8ddbd522d87f72d220ed0
luigideluca <basketteur860@gmail.com>
2021-05-27 12:13:47 +0200

    Add some integration tests to WalletController

--- ecommerce/src/test/kotlin/it/polito/ecommerce/integration/WalletControllerIntTests.kt
+++ ecommerce/src/test/kotlin/it/polito/ecommerce/integration/WalletControllerIntTests.kt
@@ -1,5 +1,9 @@
 package it.polito.ecommerce.integration
 
+import it.polito.ecommerce.common.Rolename
+import it.polito.ecommerce.domain.Customer
+import it.polito.ecommerce.domain.User
+import it.polito.ecommerce.dto.CustomerDTO
 import it.polito.ecommerce.security.JwtUtils
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -13,6 +17,7 @@ import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
 import org.springframework.web.context.WebApplicationContext
@@ -49,5 +54,65 @@ class WalletControllerIntTests @Autowired constructor(
     fun `Alice cannot retrieve other customers wallet`(){
         mockMvc.perform(get("/wallet/8").header("Authorization", "Bearer $token"))
             .andExpect(status().isForbidden)
+    }
+
+    @Test
+    fun `Alice can create a wallet`(){
+        val result = mockMvc.perform(post("/wallet/").header("Authorization", "Bearer $token").contentType("application/json").content("{\"id\": \"4\"}"))
+            .andExpect(status().isCreated)
+            .andExpect(content().contentType("application/json"))
+            .andReturn()
+        println(result.response.contentAsString)
+    }
+
+    @Test
+    fun `Alice cannot create a wallet`(){
+        mockMvc.perform(post("/wallet/").header("Authorization", "Bearer $token").contentType("application/json").content("{\"id\": \"5\"}"))
+            .andExpect(status().isForbidden)
+    }
+
+    @Test
+    fun `Alice can do a transaction`(){
+        val result = mockMvc.perform(post("/wallet/7/transaction").header("Authorization", "Bearer $token").contentType("application/json").content("{\"receiverID\": \"8\", \"amount\": \"10.5\"}"))
+            .andExpect(status().isCreated)
+            .andExpect(content().contentType("application/json"))
+            .andReturn()
+        println(result.response.contentAsString)
+    }
+
+    @Test
+    fun `Alice cannot do a transaction negative`(){
+        mockMvc.perform(post("/wallet/7/transaction").header("Authorization", "Bearer $token").contentType("application/json").content("{\"receiverID\": \"8\", \"amount\": \"-10.5\"}"))
+            .andExpect(status().isUnprocessableEntity)
+    }
+
+    @Test
+    fun `Alice cannot do a transaction to herself`(){
+        mockMvc.perform(post("/wallet/7/transaction").header("Authorization", "Bearer $token").contentType("application/json").content("{\"receiverID\": \"7\", \"amount\": \"10.5\"}"))
+            .andExpect(status().isBadRequest)
+    }
+
+    @Test
+    fun `Alice cannot do a transaction too much amount`(){
+        mockMvc.perform(post("/wallet/7/transaction").header("Authorization", "Bearer $token").contentType("application/json").content("{\"receiverID\": \"7\", \"amount\": \"70.5\"}"))
+            .andExpect(status().isBadRequest)
+    }
+
+    @Test
+    fun `Alice cannot do a transaction receiver doesn't exist`(){
+        mockMvc.perform(post("/wallet/7/transaction").header("Authorization", "Bearer $token").contentType("application/json").content("{\"receiverID\": \"6\", \"amount\": \"70.5\"}"))
+            .andExpect(status().isBadRequest)
+    }
+
+    @Test
+    fun `Alice cannot do a transaction not her wallet`(){
+        mockMvc.perform(post("/wallet/8/transaction").header("Authorization", "Bearer $token").contentType("application/json").content("{\"receiverID\": \"9\", \"amount\": \"70.5\"}"))
+            .andExpect(status().isForbidden)
+    }
+
+    @Test
+    fun `Alice cannot do a transaction sender negative`(){
+        mockMvc.perform(post("/wallet/-7/transaction").header("Authorization", "Bearer $token").contentType("application/json").content("{\"receiverID\": \"9\", \"amount\": \"70.5\"}"))
+            .andExpect(status().isUnprocessableEntity)
     }
 }
---
+++ ecommerce/src/test/kotlin/it/polito/ecommerce/unit/WalletControllerUnitTests.kt
@@ -0,0 +1,63 @@
+package it.polito.ecommerce.unit
+
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import it.polito.ecommerce.controllers.AuthController
+import it.polito.ecommerce.controllers.WalletController
+import it.polito.ecommerce.dto.RegistrationDTO
+import it.polito.ecommerce.dto.UserDetailsDTO
+import it.polito.ecommerce.repositories.EmailVerificationTokenRepository
+import it.polito.ecommerce.repositories.UserRepository
+import it.polito.ecommerce.security.JwtAuthenticationTokenFilter
+import it.polito.ecommerce.security.JwtUtils
+import it.polito.ecommerce.security.MethodSecurityConfig
+import it.polito.ecommerce.services.MailServiceImpl
+import it.polito.ecommerce.services.NotificationServiceImpl
+import it.polito.ecommerce.services.UserDetailsServiceExtImpl
+import it.polito.ecommerce.services.WalletServiceImpl
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.FilterType
+import org.springframework.context.annotation.Lazy
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.web.servlet.function.EntityResponse
+
+@WebMvcTest(
+    WalletController::class,
+    excludeFilters = [ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value =
+    [WebSecurityConfigurer::class, MethodSecurityConfig::class, JwtAuthenticationTokenFilter::class])],
+    excludeAutoConfiguration = [SecurityAutoConfiguration::class, SecurityFilterAutoConfiguration::class])
+class WalletControllerUnitTests(@Autowired private val walletController: WalletController) {
+
+    @MockkBean
+    private lateinit var walletServiceImpl: WalletServiceImpl
+
+    @Test
+    fun `Assert get wallet successfully retrieves existing wallet`(){
+        val registrationDTO = RegistrationDTO(
+            username = "pippo",
+            password = "Pippos_password1",
+            confirmPassword = "Pippos_password1",
+            email = "pippo@mail.com"
+        )
+        val userDetailsDTO = UserDetailsDTO(
+            1,
+            "pippo",
+            "bcrypt_hashed_pw",
+            true,
+            "a@b.com",
+            "CUSTOMER"
+        )
+        val out = ResponseEntity(userDetailsDTO, HttpStatus.CREATED)
+        //every { userDetailsServiceExtImpl.registerUser(registrationDTO) } returns userDetailsDTO
+        //assert(authController.register(registrationDTO) == out)
+    }
+}
