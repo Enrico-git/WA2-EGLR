db785f5bec1493ada7ae6dc105c9287aa1515f20
thepassenger <thepassenger@debian-laptop.home>
2021-07-20 13:38:46 +0200

    add update order status and delete order endpoints

--- ecommerce_final/order-service/src/main/kotlin/it/polito/wa2/orderservice/controllers/OrderController.kt
+++ ecommerce_final/order-service/src/main/kotlin/it/polito/wa2/orderservice/controllers/OrderController.kt
@@ -2,14 +2,14 @@ package it.polito.wa2.orderservice.controllers
 
 import it.polito.wa2.orderservice.dto.OrderDTO
 import it.polito.wa2.orderservice.services.OrderService
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import org.bson.types.ObjectId
+import org.springframework.dao.OptimisticLockingFailureException
 import org.springframework.data.domain.Pageable
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/orders")
@@ -20,7 +20,7 @@ class OrderController(
     /**
      * API endpoint to retrieve all the orders of the users
      * @param pageable the pagination details
-     * @return flow of OrderDTO
+     * @return the stream of orders object
      */
     @GetMapping("", produces = [MediaType.APPLICATION_NDJSON_VALUE])
     suspend fun getOrders(pageable: Pageable): Flow<OrderDTO> = orderService.getOrders(pageable)
@@ -28,8 +28,48 @@ class OrderController(
     /**
      * API endpoint to retrieve the order by its ID
      * @param orderID the ID of the order
-     * @return OrderDTO
+     * @return the order object
      */
     @GetMapping("/{orderID}")
     suspend fun getOrderByID(@PathVariable orderID: ObjectId): OrderDTO = orderService.getOrderByID(orderID)
+
+    /**
+     * API endpoint to update the order by its ID
+     * @param orderID the id of the order
+     * @param orderDTO JSON object with only the new status field
+     * @return the updated object
+     */
+     @PatchMapping("/{orderID}")
+     @ResponseStatus(HttpStatus.CREATED)
+     suspend fun updateOrderByID(@PathVariable orderID: ObjectId, @RequestBody orderDTO: OrderDTO) : OrderDTO {
+        var counter = 5
+        while (counter-- > 0){
+            try {
+                return orderService.updateOrder(orderID, orderDTO)
+            }
+            catch(e: OptimisticLockingFailureException){
+                delay(1000)
+            }
+        }
+        throw OptimisticLockingFailureException("Order")
+    }
+
+    /**
+     * API endpoint to delete the order by its ID
+     * @param orderID the id of the order
+     */
+     @DeleteMapping("/{orderID}")
+     @ResponseStatus(HttpStatus.NO_CONTENT)
+     suspend fun deleteOrderByID(@PathVariable orderID: ObjectId) {
+        var counter = 5
+        while (counter-- > 0){
+            try {
+                return orderService.deleteOrder(orderID)
+            }
+            catch(e: OptimisticLockingFailureException){
+                delay(1000)
+            }
+        }
+        throw OptimisticLockingFailureException("Order")
+    }
 }
--- ecommerce_final/order-service/src/main/kotlin/it/polito/wa2/orderservice/services/OrderServiceImpl.kt
+++ ecommerce_final/order-service/src/main/kotlin/it/polito/wa2/orderservice/services/OrderServiceImpl.kt
@@ -1,10 +1,13 @@
 package it.polito.wa2.orderservice.services
 
+import it.polito.wa2.orderservice.common.OrderStatus
 import it.polito.wa2.orderservice.domain.Order
 import it.polito.wa2.orderservice.domain.toDTO
 import it.polito.wa2.orderservice.dto.OrderDTO
 import it.polito.wa2.orderservice.dto.UserDetailsDTO
+import it.polito.wa2.orderservice.exceptions.InvalidOperationException
 import it.polito.wa2.orderservice.exceptions.NotFoundException
+import it.polito.wa2.orderservice.exceptions.UnauthorizedException
 import it.polito.wa2.orderservice.repositories.orders.OrderRepository
 import it.polito.wa2.orderservice.repositories.users.UserRepository
 import kotlinx.coroutines.flow.*
@@ -13,9 +16,11 @@ import org.bson.types.ObjectId
 import org.springframework.data.domain.Pageable
 import org.springframework.security.core.context.ReactiveSecurityContextHolder
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import java.util.logging.Logger
 
 @Service
+@Transactional
 class OrderServiceImpl(
     private val orderRepository: OrderRepository,
     private val userRepository: UserRepository,
@@ -27,7 +32,24 @@ class OrderServiceImpl(
     }
 
     override suspend fun getOrderByID(orderID: ObjectId): OrderDTO {
-        val order = orderRepository.findById(orderID) ?: throw NotFoundException("Product was not found")
+        val order = orderRepository.findById(orderID) ?: throw NotFoundException("Order was not found")
         return order.toDTO()
+    }
+
+    override suspend fun updateOrder(orderID: ObjectId, orderDTO: OrderDTO): OrderDTO {
+        val order = orderRepository.findById(orderID) ?: throw IllegalArgumentException("Order not found")
+        order.status = orderDTO.status
+        return orderRepository.save(order).toDTO()
+    }
+
+    override suspend fun deleteOrder(orderID: ObjectId) {
+        val order = orderRepository.findById(orderID) ?: throw IllegalArgumentException("Order not found")
+        val user = ReactiveSecurityContextHolder.getContext().awaitFirst().authentication.principal as UserDetailsDTO
+        if ( ! user.roles!!.contains("ADMIN") && user.username != order.buyer )
+            throw UnauthorizedException("Forbidden")
+        if (order.status != OrderStatus.ISSUED)
+            throw InvalidOperationException("You cannot cancel the order anymore")
+        order.status = OrderStatus.CANCELED
+        orderRepository.save(order)
     }
 }
