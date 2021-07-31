5912eebe3d998d8fd243de405a2ae4d8f4021b0f
thepassenger <thepassenger@debian-laptop.home>
2021-07-31 15:36:48 +0200

    add rerouting of debezium messages and formatting of the payload

--- ecommerce_final/docker/script.sh
+++ ecommerce_final/docker/script.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 mongo ./mongo_bootstrap.js 
 mongo ./mongo_createUsers.js
+sudo docker cp groovy-json-3.0.8.jar kafka_connect:/kafka/connect/debezium-connector-mongodb 
+sudo docker cp groovy-3.0.8.jar kafka_connect:/kafka/connect/debezium-connector-mongodb
+sudo docker cp groovy-jsr223-3.0.8.jar kafka_connect:/kafka/connect/debezium-connector-mongodb
+sudo docker cp debezium-scripting-1.7.0.Alpha1.jar kafka_connect:/kafka/connect/debezium-connector-mongodb
 curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ -d "$(cat connector.json )"
+
+# The content based routing of debezium uses java syntax so you can write your own rules
