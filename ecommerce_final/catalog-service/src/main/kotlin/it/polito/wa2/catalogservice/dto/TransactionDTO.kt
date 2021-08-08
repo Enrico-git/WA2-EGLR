956a0f91a90e389cf8ccdae73bf387f7ed6bc4e9
luigideluca <basketteur860@gmail.com>
2021-08-08 18:31:35 +0200

    Start adding constraints on WalletDTO and TransactionDTO (don't know if correct)

--- ecommerce_final/catalog-service/src/main/kotlin/it/polito/wa2/catalogservice/dto/TransactionDTO.kt
+++ ecommerce_final/catalog-service/src/main/kotlin/it/polito/wa2/catalogservice/dto/TransactionDTO.kt
@@ -2,6 +2,7 @@ package it.polito.wa2.catalogservice.dto
 
 import java.math.BigDecimal
 import java.sql.Timestamp
+import javax.validation.constraints.NotNull
 
 data class TransactionDTO(
     var id: String?,
@@ -9,6 +10,7 @@ data class TransactionDTO(
     var walletID: String?,
     val amount: BigDecimal,
     var description: String?,
+    @field:NotNull(message = "OrderID must not be null")
     val orderID: String,
 ){
     override fun toString(): String {
