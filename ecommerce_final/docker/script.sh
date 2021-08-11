b7bc1b4782ae885ac35cf492c4d31acda6c0030e
thepassenger <thepassenger@debian-laptop.home>
2021-08-11 15:53:43 +0200

    change network name and fixed script

--- ecommerce_final/docker/script.sh
+++ ecommerce_final/docker/script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 mongo ./mongo_bootstrap.js
 mongo ./mongo_createUsers.js
-sudo docker cp groovy-json-3.0.8.jar kafka_connect:/kafka/connect/debezium-connector-mongodb
-sudo docker cp groovy-3.0.8.jar kafka_connect:/kafka/connect/debezium-connector-mongodb
-sudo docker cp groovy-jsr223-3.0.8.jar kafka_connect:/kafka/connect/debezium-connector-mongodb
-sudo docker cp debezium-scripting-1.7.0.Alpha1.jar kafka_connect:/kafka/connect/debezium-connector-mongodb
-sudo docker restart kafka_connect
+#sudo docker cp groovy-json-3.0.8.jar kafka_connect:/kafka/connect/debezium-connector-mongodb
+#sudo docker cp groovy-3.0.8.jar kafka_connect:/kafka/connect/debezium-connector-mongodb
+#sudo docker cp groovy-jsr223-3.0.8.jar kafka_connect:/kafka/connect/debezium-connector-mongodb
+#sudo docker cp debezium-scripting-1.7.0.Alpha1.jar kafka_connect:/kafka/connect/debezium-connector-mongodb
+#sudo docker restart kafka_connect
 # This links debezium with kafka
 # *Reminder* Debezium is just a trigger: it observes the table and when an event occur it performs an action.
 #curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ -d "$(cat connector.json )"
