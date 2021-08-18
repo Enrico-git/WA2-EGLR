e65e2fb0a24e0854471264c20ad1d5194a7996d9
luigideluca <basketteur860@gmail.com>
2021-08-18 13:31:14 +0200

    TODO in CustomerService removed

--- ecommerce_final/catalog-service/src/main/kotlin/it/polito/wa2/catalogservice/services/CustomerService.kt
+++ ecommerce_final/catalog-service/src/main/kotlin/it/polito/wa2/catalogservice/services/CustomerService.kt
@@ -4,7 +4,6 @@ import it.polito.wa2.catalogservice.dto.CustomerDTO
 import org.bson.types.ObjectId
 import org.springframework.security.access.prepost.PreAuthorize
 
-//TODO isUser and IsCustomer??
 interface CustomerService {
     @PreAuthorize("hasAuthority(\"CUSTOMER\") or hasAuthority(\"ADMIN\")")
     suspend fun addCustomer(customerDTO: CustomerDTO): CustomerDTO
